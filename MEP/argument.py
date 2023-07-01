6d7f25f2628b92d24cefe7c9adb222972f777db6
This-is-ITC <3467359137@qq.com>
2023-07-01 19:27:38 +0800

    初级版本

---
+++ MEP/argument.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+
+operators = {'__add__': '+',
+             '__sub__': '-',
+             '__mul__': '*',
+             '__floordiv__': '//',
+             '__div__': '/',
+             '__mod__': '%',
+             '__pow__': '**',
+             '__lshift__': '<<',
+             '__rshift__': '>>',
+             '__and__': '&',
+             '__xor__': '^',
+             '__or__': '|'}
+
+def take_function_name(func):
+    name = func.__name__
+    def wrapper(*args, **kwargs):
+        return func(*args, __name__=name, **kwargs)
+    return wrapper
+
+class Production:
+    
+    def __init__(self, func, exp):
+        self._func = func
+        self._exp = exp
+
+    def product(self, operator, left, right):
+        if type(right) == Production:
+            return Production(
+                eval(f'lambda x: left._func(x) {operators[operator]} right._func(x)',
+                     {
+                         'operators': operators,
+                          'left': left,
+                          'right': right,
+                          'operator': operator
+                      }
+                     ), 
+                str(left._exp) + operators[operator] + str(right._exp)
+                )
+        else:
+            return Production(
+                eval(f'lambda x: left._func(x) {operators[operator]} right',
+                     {
+                         'operators': operators,
+                          'left': left,
+                          'right': right,
+                          'operator': operator
+                      }
+                     ),
+                str(left._exp) + operators[operator] + str(right)
+                )
+
+    @take_function_name
+    def __add__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __sub__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __mul__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __floordiv__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __div__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __mod__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __pow__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __lshift__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __rshift__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __and__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __xor__(self, other, __name__): return self.product(__name__, self, other)
+
+    @take_function_name
+    def __or__(self, other, __name__): return self.product(__name__, self, other)
+
+X = Production(lambda x: x, 'x')
---
+++ MEP/formula.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+
+from .argument import Production, X
+
+
+class Formula:
+
+    def __init__(self, expression: Production):
+        self._func = expression._func
+        self._exp = expression._exp
+    
+    def substitute(self, arg):
+        return Expression(self._func, self._exp, arg)
+
+    def get_exp(self):
+        return self._exp
+
+class Expression:
+
+    def __init__(self, func, exp, arg):
+        self._func = func
+        self._exp = exp
+        self._arg = arg
+
+    def value(self):
+        return self._func(self._arg)
+
+    def get_exp(self):
+        exp = ''
+        for item in self._exp:
+            exp += str(self._arg) if item == 'x' else item
+        return exp
+
+
+if __name__ == '__main__':
+    f = Formula(X*2+3)
+    print(f.get_exp())
+    exp = f.substitute(4j+1.44e2)
+    print(exp.get_exp())
+    print(exp.value())
